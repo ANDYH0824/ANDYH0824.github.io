1b02fe53aa627f4c444a233c227101c5d00456c4
ANDYH0824 <1637987453@qq.com>
2024-03-25 20:52:30 +0800

    Site updated: 2024-03-25 20:52:28

---
+++ anzhiyu/random.js
@@ -0,0 +1,1 @@
+var posts=["posts/4a17b156.html","posts/ea1f0973.html","posts/dfb8a81f.html","posts/5fd6e59a.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
