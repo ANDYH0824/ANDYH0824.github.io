e010cf40bb0563b71d322e34b23229ff1bd70a77
ANDYH0824 <1637987453@qq.com>
2024-05-03 23:19:56 +0800

    Site updated: 2024-05-03 23:19:52

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/4a17b156.html","posts/ea1f0973.html","posts/dfb8a81f.html","posts/96a41905.html","posts/5fd6e59a.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/4a17b156.html","posts/dfb8a81f.html","posts/ea1f0973.html","posts/96a41905.html","posts/5fd6e59a.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
